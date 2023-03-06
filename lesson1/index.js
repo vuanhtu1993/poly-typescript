ac66468e1f44688f8a519c9116e2c223ece15f89
tuva9 <tuva9@fpt.edu.vn>
2023-03-06 15:22:41 +0700

    lesson 1

---
+++ lesson1/index.js
@@ -0,0 +1,9 @@
+// Hello world
+// const greeting = "Xin chào cả lớp"
+// console.log(greeting);
+// Data types
+var a = 10;
+var str = "Hello world";
+var trangthai = true;
+// Explicit and Implicit
+var b = 20;
