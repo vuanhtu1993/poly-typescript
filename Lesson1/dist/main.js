6e1fc7e00bc6a92da7ef01c2035260684eb620b1
tuva9 <tuva9@fpt.edu.vn>
2023-03-07 12:53:49 +0700

    Buổi 1

---
+++ Lesson1/dist/main.js
@@ -0,0 +1,11 @@
+var greeting = "Xin chao ca lop";
+// console.log(greeting);
+// Explicit
+var str = "Hello world";
+console.log(str);
+// Implicit
+var a = 10;
+// a = "Hello world"
+if (typeof str == "number") {
+    console.log(str - a);
+}
---
+++ Lesson1/main.ts
@@ -0,0 +1,20 @@
+const greeting: string = "Xin chao ca lop"
+
+// console.log(greeting);
+
+
+
+
+// Explicit
+let str: string = "Hello world"
+console.log(str);
+
+// Implicit
+const a = 10;
+// a = "Hello world"
+if (typeof str == "number") {
+    console.log(str - a);
+}
+
+
+
