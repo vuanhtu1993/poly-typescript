8d0cf0ab9e888939819dc610fdd87e0edd92a69f
tuva9 <tuva9@fpt.edu.vn>
2023-03-11 11:23:42 +0700

    Buổi 3: Object

---
+++ Lesson2/dist/dataTypes/object.js
@@ -0,0 +1,17 @@
+// Object declaration
+var sinhvien = {
+    name: "Tài",
+    grade: 5,
+    age: 20,
+    hometown: "Thái Bình"
+};
+var sinhvien2 = {
+    name: "Thanh",
+    grade: 5,
+    age: 20,
+    hometown: "Nam Định"
+};
+// const book1: Book = {
+// }
+// const book2: Book = {
+// }
---
+++ Lesson2/src/dataTypes/object.ts
@@ -0,0 +1,44 @@
+// Object declaration
+const sinhvien: { name: string, grade: number, hometown: string, age: number } = {
+    name: "Tài",
+    grade: 5,
+    age: 20,
+    hometown: "Thái Bình",
+}
+
+const sinhvien2: { name: string, grade: number, hometown: string, age: number } = {
+    name: "Thanh",
+    grade: 5,
+    age: 20,
+    hometown: "Nam Định",
+}
+
+// const book: {authors: {id: number, name: string, slug: string}[], book_cover?: string, categories: {id: number, name: string, is_leaf: boolean}}
+interface IBook {
+    authors: Author[],
+    book_cover?: string,
+    categories: { id: number, name: string, is_leaf: boolean },
+    description: string,
+    images: Image[]
+}
+
+interface Author {
+    id: number,
+    name: string,
+    slug: string
+}
+
+interface Image {
+    base_url: string,
+    is_gallery: boolean,
+    label: string,
+    large_url: string
+}
+
+// const book1: Book = {
+
+// }
+
+// const book2: Book = {
+    
+// }
