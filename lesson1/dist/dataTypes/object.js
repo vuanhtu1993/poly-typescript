4233ef7fa69353a156fa1b2b21979cd00b76a8dc
tuva9 <tuva9@fpt.edu.vn>
2023-03-13 14:48:23 +0700

    Buổi 4: Type aliases

--- lesson1/dist/dataTypes/object.js
+++ lesson1/dist/dataTypes/object.js
@@ -8,3 +8,10 @@ var sinhvien2 = {
     name: "Thành",
     age: 20
 };
+// Example
+var ROLE;
+(function (ROLE) {
+    ROLE[ROLE["STAFF"] = 0] = "STAFF";
+    ROLE[ROLE["MANAGER"] = 1] = "MANAGER";
+    ROLE[ROLE["USER"] = 2] = "USER";
+})(ROLE || (ROLE = {}));
