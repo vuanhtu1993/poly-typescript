45a5731f6700a6fc3a2768504903446f1820ac03
tuva9 <tuva9@fpt.edu.vn>
2023-03-15 16:46:18 +0700

    Buổi 5: Lab 1

--- lesson1/dist/dataTypes/generic.js
+++ lesson1/dist/dataTypes/generic.js
@@ -19,15 +19,34 @@ var arrStr = ["Quang", "Quy", "Hau", "Duc"]; //Fibonacci
 //     }
 //     return temp
 // }
-function we17304_map(arr, callback) {
-    var temp = [];
-    for (var i = 0; i < arr.length; i++) {
-        var newItem = callback(arr[i]);
-        temp.push(newItem);
-    }
-    return temp;
-}
-var result = we17304_map(arrStr, function (item) {
-    return item + " - we17304";
+// function we17304_map<T>(arr: T[], callback: (item: T) => T): T[] {
+//     let temp = []
+//     for(let i = 0; i < arr.length; i++) {
+//         const newItem = callback(arr[i])
+//         temp.push(newItem)
+//     }
+//     return temp
+// }
+// const result = we17304_map(arrStr, (item) => {
+//     return item + " - we17304"
+// })
+// console.log(result);
+var arrNum2 = [13, 1, 3, 5, 8, 2]; //Fibonacci
+var arrStr2 = ["b", "cb", "ab", "e"];
+arrNum2.sort(function (a, b) {
+    return b - a;
 });
-console.log(result);
+// console.log(arrNum2);
+function selectionSort(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+selectionSort(arrStr2);
+console.log(arrStr2);
--- lesson1/src/dataTypes/generic.ts
+++ lesson1/src/dataTypes/generic.ts
@@ -25,17 +25,41 @@ const arrStr = ["Quang","Quy","Hau","Duc"] //Fibonacci
 //     return temp
 // }
 
-function we17304_map<T>(arr: T[], callback: (item: T) => T): T[] {
-    let temp = []
-    for(let i = 0; i < arr.length; i++) {
-        const newItem = callback(arr[i])
-        temp.push(newItem)
-    }
-    return temp
-}
+// function we17304_map<T>(arr: T[], callback: (item: T) => T): T[] {
+//     let temp = []
+//     for(let i = 0; i < arr.length; i++) {
+//         const newItem = callback(arr[i])
+//         temp.push(newItem)
+//     }
+//     return temp
+// }
 
-const result = we17304_map(arrStr, (item) => {
-    return item + " - we17304"
+// const result = we17304_map(arrStr, (item) => {
+//     return item + " - we17304"
+// })
+
+// console.log(result);
+
+const arrNum2 = [13,1,3,5,8,2] //Fibonacci
+const arrStr2 = ["b", "cb", "ab", "e"]
+arrNum2.sort((a, b) => {
+    return b - a
 })
 
-console.log(result);
+// console.log(arrNum2);
+
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length - 1; i ++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+                let temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+}
+
+selectionSort(arrStr2)
+console.log(arrStr2);
+
