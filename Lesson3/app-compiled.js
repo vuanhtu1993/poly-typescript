c14037ff954ebbabc8a3680dd66049e108ffb05a
tuva9 <tuva9@fpt.edu.vn>
2023-03-16 23:22:59 +0700

    Pure react

--- Lesson3/app-compiled.js
+++ Lesson3/app-compiled.js
@@ -1,3 +1,16 @@
 const root = ReactDOM.createRoot(document.getElementById('app'));
-const element = /*#__PURE__*/React.createElement("h1", null, "Hello, world");
+const products = [{
+  name: "San pham 1",
+  price: 100
+}, {
+  name: "San pham 2",
+  price: 200
+}, {
+  name: "San pham 3",
+  price: 300
+}, {
+  name: "San pham 4",
+  price: 400
+}];
+const element = /*#__PURE__*/React.createElement(React.Fragment, null, products.map(item => /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, item.name), /*#__PURE__*/React.createElement("p", null, item.price))));
 root.render(element);
--- Lesson3/app.js
+++ Lesson3/app.js
@@ -1,3 +1,17 @@
 const root = ReactDOM.createRoot(document.getElementById('app'));
-const element = <h1>Hello, world</h1>;
+
+const products = [
+    { name: "San pham 1", price: 100 },
+    { name: "San pham 2", price: 200 },
+    { name: "San pham 3", price: 300 },
+    { name: "San pham 4", price: 400 },
+]
+const element = <>
+    {products.map(item =>
+        <div>
+            <h2>{item.name}</h2>
+            <p>{item.price}</p>
+        </div>
+    )}
+</>;
 root.render(element);
