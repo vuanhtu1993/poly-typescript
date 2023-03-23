5d43f230328e88c6dc2f5e2d9a3a7c493e48231e
tuva9 <tuva9@fpt.edu.vn>
2023-03-23 07:54:08 +0700

    Buổi 7: Create react app

---
+++ lesson4/src/index.js
@@ -0,0 +1,19 @@
+// Bước 1: import react
+import React from 'react'
+import ReactDOM from 'react-dom'
+// Bước 2: Khai báo DOM
+const root = document.querySelector("#root")
+// Bước 3: Tạo React DOM
+const reactRoot = ReactDOM.createRoot(root)
+// Bước 4: Render
+// JSX
+const App = () => {
+    return <h1>Hello world</h1>
+}
+reactRoot.render(<App/>)
+
+// Cac buoc cai dat
+// 1. npm install -g create-react-app
+// 2. npx create-react-app <ten folder>
+// 3. Xoa cac thu muc trong src
+// 4. Tao file index.js
