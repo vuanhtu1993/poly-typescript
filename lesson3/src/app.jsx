0dfbeb8920c2e095d3b362b355e422ee247752fd
tuva9 <tuva9@fpt.edu.vn>
2023-03-29 15:11:17 +0700

    Buổi 11: useEffect

--- lesson3/src/app.jsx
+++ lesson3/src/app.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react"
 import Square from "./components/square"
 import Board from "./components/board"
+import Counter from "./components/counter"
 
 // Props: Là một đối tượng mà dùng để truyền thông tin từ Component cha xuống component con
 // State: Là trạng thái (bộ nhớ) của component, nó quyết định componetn được hiện thị như thế nào
@@ -9,9 +10,7 @@ const App = () => {
 
     return <div
         className="h-[100vh] flex flex-col justify-center items-center">
-        <Board>
-            
-        </Board>
+        <Counter/>
     </div>
 }
 
