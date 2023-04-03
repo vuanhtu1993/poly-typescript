d79e79b4099ed6bc31d4aa38866504b15087850a
tuva9 <tuva9@fpt.edu.vn>
2023-04-03 14:24:48 +0700

    Buổi 13: Signup

--- assignment2/src/App.tsx
+++ assignment2/src/App.tsx
@@ -3,11 +3,13 @@ import {BrowserRouter, Router, Routes, Route} from 'react-router-dom'
 import UserLayout from './components/userLayout'
 import Home from './pages/home'
 import ProductDetail from './pages/product'
+import Signup from './pages/signup'
 
 function App() {
 
   return <BrowserRouter>
     <Routes>
+      <Route path='/signup' element={<Signup/>}/>
       <Route path='/' element={<UserLayout/>}> {/* Layout */}
         <Route index element={<Home/>}/>
         <Route path='product/:id' element={<ProductDetail/>}/>
