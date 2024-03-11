1b92e447a6e2da5e05a362bc99c002d834dd46c3
tuva9 <tuva9@fpt.edu.vn>
2024-03-11 09:03:31 +0700

    Buổi 2: Datatype

--- lesson2/datatype.js
+++ lesson2/datatype.js
@@ -4,77 +4,50 @@
 // String, Number, Boolean, Null, Underfine, Symbol
 // Non Primitive
 // Object, Array, Function
-
 // Datatype in Typescript
 // Primitive
 // string, number, boolean
 // Anotation
-let myName: string = "tuva9"
-myName = "LinhNV"
+var myName = "tuva9";
+myName = "LinhNV";
 console.log(myName.toLowerCase());
 // Inference
-let name2 = "LinhNV"
-
+var name2 = "LinhNV";
 // null, undefined
-let name3: undefined;
+var name3;
 console.log(name3);
-
 // Object type
-const user1: {
-    name: string,
-    age: number,
-    gender: boolean
-} = {
+var user1 = {
     name: "tuva",
     age: 31,
     gender: true
-}
-
-const user2: {
-    name: string,
-    age: number,
-    gender: boolean
-} = {
+};
+var user2 = {
     name: "HoangTH",
     age: 21,
     gender: true
-}
-
+};
 // Array type
-const array1 = ["A", "B", "C", "D", 1]
-const array2: string[] = ["A", "B", "C", "D", "E"]
-const array3: Array<number> = [1, 2, 3, 4, 5, 6]
-
-// Interface type
-interface User {
-    name: string,
-    age: number,
-    gender: boolean,
-    role: number
-}
+var array1 = ["A", "B", "C", "D", 1];
+var array2 = ["A", "B", "C", "D", "E"];
+var array3 = [1, 2, 3, 4, 5, 6];
 // const user3: User = {
 //     name: "ToiNH",
 //     age: 21,
 //     gender: true
 // }
-
 // Enum type
 // Role 1 là quản trị, 0 là khách hàng, 2 là superAdmin
-enum ROLE {
-    "USER" = 1,
-    "ADMIN",
-    "SUPERADMIN",
-}
-
-const user4: User = {
+var ROLE;
+(function (ROLE) {
+    ROLE[ROLE["USER"] = 0] = "USER";
+    ROLE[ROLE["ADMIN"] = 1] = "ADMIN";
+    ROLE[ROLE["SUPERADMIN"] = 2] = "SUPERADMIN";
+})(ROLE || (ROLE = {}));
+var user4 = {
     name: "VietVT",
     age: 21,
     gender: true,
     role: ROLE.USER
-}
-
+};
 console.log(user4);
-
-// Tuple
-const tuple1: [string, number, boolean, number] = ["tuva", 31, true, ROLE.ADMIN]
-
