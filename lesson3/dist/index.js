53f5e0264176a981be224531fee9d45932e4b09c
tuva9 <tuva9@fpt.edu.vn>
2024-03-14 17:08:24 +0700

    Buổi 4: Type guard and narrowing

--- lesson3/dist/index.js
+++ lesson3/dist/index.js
@@ -1,9 +1,31 @@
 "use strict";
-console.log("index file 2");
-function sum(a, b) {
-    return a + b;
-}
+console.log("Hello world 1");
 const btn = document.querySelector("#btn");
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', function () {
-    console.log("abc");
+// Type guards/ Narrowing
+// 1. typeof
+// 2. instanceof
+// 3. Truthiness narrowing - Boolean negations (!) && ||
+const handleClick = (dom, callback) => {
+    // typeof
+    if (typeof dom == "string") {
+        const element = document.querySelector(dom);
+        element === null || element === void 0 ? void 0 : element.addEventListener('click', () => {
+            // Truthiness
+            if (callback) {
+                callback(element);
+            }
+            // callback?.(element)
+        });
+        // instanceof 
+    }
+    else if (dom instanceof Element) {
+        dom.addEventListener("click", () => {
+            if (callback) {
+                callback(dom);
+            }
+        });
+    }
+};
+handleClick("#btn", (element) => {
+    console.log(element.textContent);
 });
--- lesson3/src/index.ts
+++ lesson3/src/index.ts
@@ -1,10 +1,32 @@
-console.log("index file 2");
-function sum(a: number, b: number) {
-    return a + b
+console.log("Hello world 1");
+const btn = document.querySelector("#btn")
+
+// Type guards/ Narrowing
+// 1. typeof
+// 2. instanceof
+// 3. Truthiness narrowing - Boolean negations (!) && ||
+
+const handleClick = (dom: Element | string | null, callback?: (element: Element) => void) => {
+    // typeof
+    if (typeof dom == "string") {
+        const element = document.querySelector(dom)
+        element?.addEventListener('click', () => {
+            // Truthiness
+            if (callback) {
+                callback(element)
+            }
+            // callback?.(element)
+        })
+        // instanceof 
+    } else if (dom instanceof Element) {
+        dom.addEventListener("click", () => {
+            if (callback) {
+                callback(dom)
+            }
+        })
+    }
 }
 
-const btn = document.querySelector("#btn")
-btn?.addEventListener('click', function () {
-    console.log("abc");
-
+handleClick("#btn", (element) => {
+    console.log(element.textContent);
 })
