17f1f72ebe76f48272188d0daf4b0f341fc57dbf
tuva9 <tuva9@fpt.edu.vn>
2024-03-15 10:52:58 +0700

    Buổi 4: Type guard

--- lesson3/src/index.ts
+++ lesson3/src/index.ts
@@ -4,3 +4,34 @@ enum STATUS {
 }
 
 console.log("Hello world 2");
+
+// Type guard/ Narrowing
+// 1. Truthiness - Check null and undefined (if else - ? - &&)
+// 2. Type checked
+// - typeof
+// - instanceof
+
+const btn = document.querySelector("#btn")
+// btn?.addEventListener("click", () => {
+//     alert(btn.textContent)
+// })
+
+const handleClick = (element: Element | string | null, callback?: () => void) => {
+    if (typeof element == "string") {
+        const dom = document.querySelector(element)
+        dom?.addEventListener("click", () => {
+            callback()
+        })
+    } else if (element instanceof Element) {
+        element?.addEventListener("click", () => {
+            callback()
+        })
+    }
+
+
+}
+
+
+handleClick(btn, () => {
+    alert("handle click is working")
+})
