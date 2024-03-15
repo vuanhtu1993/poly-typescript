a3481ae9f0d4eee8b1e44bf72e3bce7150d4b0e3
tuva9 <tuva9@fpt.edu.vn>
2024-03-15 08:53:04 +0700

    Buổi 4: Type guard

--- lesson3/dist/index.js
+++ lesson3/dist/index.js
@@ -3,16 +3,22 @@ console.log("Hello world 4");
 const btn = document.querySelector("#btn");
 // Type guard / Narrowing
 // 1. Truthiness 
+// 2. Type checked
+// - instanceof 
+// - typeof
 const handleClick = (element, callback) => {
-    // if (element) {
-    // element.addEventListener("click", () => {
-    //     callback()
-    // })
-    // }
-    element === null || element === void 0 ? void 0 : element.addEventListener("click", () => {
-        callback();
-    });
+    if (typeof element == "string") {
+        const dom = document.querySelector(element);
+        dom === null || dom === void 0 ? void 0 : dom.addEventListener('click', () => {
+            callback();
+        });
+    }
+    else if (element instanceof Element) {
+        element === null || element === void 0 ? void 0 : element.addEventListener("click", () => {
+            callback();
+        });
+    }
 };
-handleClick(btn, () => {
+handleClick("#btn", () => {
     console.log("handle click is working");
 });
--- lesson3/src/index.ts
+++ lesson3/src/index.ts
@@ -4,17 +4,23 @@ const btn = document.querySelector("#btn")
 
 // Type guard / Narrowing
 // 1. Truthiness 
-const handleClick = (element: Element | null, callback: () => void) => {
-    // if (element) {
-    // element.addEventListener("click", () => {
-    //     callback()
-    // })
-    // }
-    element?.addEventListener("click", () => {
-        callback()
-    })
+// 2. Type checked
+// - instanceof 
+// - typeof
+const handleClick = (element: Element | string | null, callback: () => void) => {
+    if (typeof element == "string") {
+        const dom = document.querySelector(element)
+        dom?.addEventListener('click', () => {
+            callback()
+        })
+    } else if (element instanceof Element) {
+        element?.addEventListener("click", () => {
+            callback()
+        })
+    }
+
 }
 
-handleClick(btn, () => {
+handleClick("#btn", () => {
     console.log("handle click is working");
 })
