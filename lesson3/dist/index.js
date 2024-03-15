b76d414a4ae27e8d35fcf8de9d5d62a29d2c1610
tuva9 <tuva9@fpt.edu.vn>
2024-03-15 08:45:42 +0700

    Buổi 4: Type guard

--- lesson3/dist/index.js
+++ lesson3/dist/index.js
@@ -1,6 +1,18 @@
 "use strict";
 console.log("Hello world 4");
 const btn = document.querySelector("#btn");
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
-    alert(btn.textContent);
+// Type guard / Narrowing
+// 1. Truthiness 
+const handleClick = (element, callback) => {
+    // if (element) {
+    // element.addEventListener("click", () => {
+    //     callback()
+    // })
+    // }
+    element === null || element === void 0 ? void 0 : element.addEventListener("click", () => {
+        callback();
+    });
+};
+handleClick(btn, () => {
+    console.log("handle click is working");
 });
--- lesson3/src/index.ts
+++ lesson3/src/index.ts
@@ -1,6 +1,20 @@
 console.log("Hello world 4");
 
 const btn = document.querySelector("#btn")
-btn?.addEventListener("click", () => {
-    alert(btn.textContent)
+
+// Type guard / Narrowing
+// 1. Truthiness 
+const handleClick = (element: Element | null, callback: () => void) => {
+    // if (element) {
+    // element.addEventListener("click", () => {
+    //     callback()
+    // })
+    // }
+    element?.addEventListener("click", () => {
+        callback()
+    })
+}
+
+handleClick(btn, () => {
+    console.log("handle click is working");
 })
