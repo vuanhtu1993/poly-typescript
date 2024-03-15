9781d9a50f67d6a56d6971b24a9d8ac846ff9a0a
tuva9 <tuva9@fpt.edu.vn>
2024-03-15 10:09:14 +0700

    Buổi 4: TSConfig

--- lesson3/dist/index.js
+++ lesson3/dist/index.js
@@ -1,31 +1,7 @@
 "use strict";
-console.log("Hello world 1");
-const btn = document.querySelector("#btn");
-// Type guards/ Narrowing
-// 1. typeof
-// 2. instanceof
-// 3. Truthiness narrowing - Boolean negations (!) && ||
-const handleClick = (dom, callback) => {
-    // typeof
-    if (typeof dom == "string") {
-        const element = document.querySelector(dom);
-        element === null || element === void 0 ? void 0 : element.addEventListener('click', () => {
-            // Truthiness
-            if (callback) {
-                callback(element);
-            }
-            // callback?.(element)
-        });
-        // instanceof 
-    }
-    else if (dom instanceof Element) {
-        dom.addEventListener("click", () => {
-            if (callback) {
-                callback(dom);
-            }
-        });
-    }
-};
-handleClick("#btn", (element) => {
-    console.log(element.textContent);
-});
+var STATUS;
+(function (STATUS) {
+    STATUS[STATUS["NOTFOUND"] = 404] = "NOTFOUND";
+    STATUS[STATUS["INTERNAL SERVER"] = 500] = "INTERNAL SERVER";
+})(STATUS || (STATUS = {}));
+console.log("Hello world 2");
